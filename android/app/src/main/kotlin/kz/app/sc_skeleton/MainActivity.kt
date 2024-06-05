154c8969b38c98ba19789d58ea236687a5addcb1
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-06-06 02:27:15 +0500

    . r platform info changes

--- android/app/src/main/kotlin/kz/app/sc_skeleton/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package kz.app.sc_skeleton
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/core/_shared/constants/app_constants.dart
+++ lib/core/_shared/constants/app_constants.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/foundation.dart';
+
 /// `AppConstants` is a class that holds all the base `static` constants used in the app.
 final class AppConstants {
   /// TODO: Change this to your app name
-  static const String appName = 'Module template';
+  static const String appName = 'Modules Template';
 
   /// TODO: Change this to your app identifier
-  static const String appIdentifier = 'kz.module.template';
+  static String appIdentifier = defaultTargetPlatform == TargetPlatform.android
+      ? 'kz.app.modules_template'
+      : 'kz.app.modulesTemplate';
 }
